85f3ebe1570a28dc5ada6861614b04b3fda3c1e7
Daniel Loewenstein <loewenstein.daniel@gmail.com>
2018-09-04 12:59:19 +0200

    fixed error in shiny-server.sh

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,16 +1,16 @@
-#!binsh
+#!/bin/sh
 
 # Make sure the directory for individual app logs exists
-mkdir -p varlogshiny-server
-chown shiny.shiny varlogshiny-server
+mkdir -p /var/log/shiny-server
+chown shiny.shiny /var/log/shiny-server
 
-if [ $APPLICATION_LOGS_TO_STDOUT = false ];
+if [ "$APPLICATION_LOGS_TO_STDOUT" = "false" ];
 then
-    exec shiny-server 2&1
+    exec shiny-server 2>&1
 else
     # start shiny server in detached mode
-    exec shiny-server 2&1 &
+    exec shiny-server 2>&1 &
 
-    # push the real application logs to stdout with xtail
-    exec xtail varlogshiny-server
+    # push the "real" application logs to stdout with xtail
+    exec xtail /var/log/shiny-server/
 fi
