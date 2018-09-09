1447678ddef50f6068f5e703016359aba7dbe01e
Daniel Loewenstein <loewenstein.daniel@gmail.com>
2018-09-09 18:50:21 +0200

    custom renderer for cellbackground

---
+++ echoewaves_app/R/color_renderer.R
@@ -0,0 +1,4 @@
+color_renderer <- "function(instance, td, row, col, prop, value, cellProperties){
+                  Handsontable.renderers.TextRenderer.apply(this, arguments);
+                  td.style.backgroundColor = 'lightgreen'
+                  }"
